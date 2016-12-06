8797af6965bf65dd589896d82ba714d184f285dd
Christo de Klerk <deklerk@gmail.com>
2016-12-06 09:05:06 -0800

    powered by bordr

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 						<a href="http://ec.europa.eu/programmes/creative-europe/projects/ce-project-details-page/?nodeRef=workspace://SpacesStore/92784398-e6d7-4950-b1e4-3bcde44e03fe" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
 					<!-- .site-info --></div>
 					<div class="col-sm-6 col-md-6 site-info">
-						powered by <a href="http://bordr.org" target="_blank">Bordr.org</a>
 					<!-- .site-info --></div>
 					<div class="col-sm-6 col-md-3">
 						<i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a><br/>
 						<i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a><br/>
 						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a><br/>
+						<i class="fa fa-bolt" aria-hidden="true"></i> powered by <a href="http://bordr.org" target="_blank">Bordr.org</a>
 					<!-- .site-credit --></div>
 				</div>
 			</div>
