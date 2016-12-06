8c603b6e8a5ff219b3ded4aeafdc21716d882b44
Christo de Klerk <deklerk@gmail.com>
2016-12-06 08:57:24 -0800

    footer fix

--- footer.php
+++ footer.php
@@ -16,8 +16,10 @@
 					<div class="col-sm-6 col-md-3 site-info">
 						<i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a><br/>
 						<i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a><br/>
-						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a>
+						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a><br/>
 						powered by <a href="http://bordr.org" target="_blank">Bordr.org</a>
+					<!-- .site-info --></div>
+					<div class="col-sm-6 col-md-3 site-info">
 					<!-- .site-info --></div>
 					<div class="col-sm-6 col-md-3 site-credit">
 						<a href="http://ec.europa.eu/programmes/creative-europe/" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
