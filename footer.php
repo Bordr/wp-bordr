f70f2475419dbcabe3cef4c2ac54713143247a68
Christo de Klerk <deklerk@gmail.com>
2016-12-06 08:55:57 -0800

    footer formatting

--- footer.php
+++ footer.php
@@ -14,15 +14,13 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-6 col-md-3 site-info">
-						<a href="http://ec.europa.eu/programmes/creative-europe/" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
-					<!-- .site-info --></div>
-					<div class="col-sm-6 col-md-6 site-info">
-						<small><i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a></small><br/>
-						<small><i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a></small><br/>
-						<small><i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a></small><br/>
-					</div>
-					<div class="col-sm-6 col-md-3 site-credit">
+						<i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a><br/>
+						<i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a><br/>
+						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a>
 						powered by <a href="http://bordr.org" target="_blank">Bordr.org</a>
+					<!-- .site-info --></div>
+					<div class="col-sm-6 col-md-3 site-credit">
+						<a href="http://ec.europa.eu/programmes/creative-europe/" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
 					<!-- .site-credit --></div>
 				</div>
 			</div>
