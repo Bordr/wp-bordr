6c54f7e7f5f5082229be0d2bd926990375794af9
Christo de Klerk <deklerk@gmail.com>
2016-12-06 08:50:20 -0800

    cc and github meta

--- footer.php
+++ footer.php
@@ -17,7 +17,9 @@
 						<a href="http://ec.europa.eu/programmes/creative-europe/" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
 					<!-- .site-info --></div>
 					<div class="col-sm-6 col-md-6 site-info">
-						This page is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike License</a>.
+						<small><i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a></small><br/>
+						<small><i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a></small><br/>
+						<small><i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a></small><br/>
 					</div>
 					<div class="col-sm-6 col-md-3 site-credit">
 						powered by <a href="http://bordr.org" target="_blank">Bordr.org</a>
