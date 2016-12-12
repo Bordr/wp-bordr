e84fa3632e151c3d397737c7557cd6aae46058ec
Christo de Klerk <deklerk@gmail.com>
2016-12-12 15:48:24 -0800

    reorganize footer

--- footer.php
+++ footer.php
@@ -13,17 +13,21 @@
 		<footer id="footer" class="site-footer" role="contentinfo">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-6 col-md-3 site-info">
+					<div class="col-sm-6 col-md-4 site-info">
 						<a href="http://ec.europa.eu/programmes/creative-europe/projects/ce-project-details-page/?nodeRef=workspace://SpacesStore/92784398-e6d7-4950-b1e4-3bcde44e03fe" target="_blank"><img src="/wp-content/uploads/2015/12/eu_flag_creative_europe_co_funded_pos_rgb_right.jpg" style="width:200px;"></a>
 					<!-- .site-info --></div>
-					<div class="col-sm-6 col-md-6 site-info">
+					<div class="col-sm-6 col-md-3 site-info">
+						<i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Help build our forms</a><br/>
+						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Help code this site</a>
+					<!-- .site-credit --></div>
+					<div class="col-sm-6 col-md-2 site-info">
+						<a href="/terms-of-service/" target="_blank">Terms of Service</a><br/>
+						<a href="/privacy-policy/" target="_blank">Privacy Policy</a><br/>
 					<!-- .site-info --></div>
 					<div class="col-sm-6 col-md-3">
-						<i class="fa fa-bolt" aria-hidden="true"></i> powered by <a href="http://bordr.org" target="_blank">Bordr.org</a><br/>
+						Europe Grand Central – 2016<br/>
 						<i class="fa fa-creative-commons" aria-hidden="true"></i> <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Attribution-ShareAlike License</a><br/>
-						<i class="fa fa-paragraph" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr/wiki" target="_blank">Build standards with us</a><br/>
-						<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/Bordr/bordr" target="_blank">Code with us</a>
-					<!-- .site-credit --></div>
+					<!-- .site-info --></div>
 				</div>
 			</div>
 		<!-- #footer --></footer>
