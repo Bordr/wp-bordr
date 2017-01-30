8240af0c0e09b68f2e8b360afc3ec95e534beeba
Felix Panozzo <fpanozzo@gmail.com>
2017-01-30 12:23:05 +0100

    Create headings of chosen methods in frontend form
    Replicated feature from the WordPress admin form
    for activities.
    
    Feedback from @haraldssonfoto:
      Attention. This does not seem to work – the ticked method boxes does not create new headings in this field…

--- activity-form-template.php
+++ activity-form-template.php
@@ -29,4 +29,19 @@ get_header();
   </main>
 </div>
 
+<script type="text/javascript">
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+  <?php /* Create headings in "How"-textarea when choosing methods */ ?>
+  $('input[name^="acf[field_570d4d3b389ea]"]').change(function() {
+    if($(this).is(':checked')) {
+      tinymce.get($('textarea[name="acf[field_5702d2889ac48]"]').attr('id'))
+             .execCommand('mceInsertContent', false, "<p><b>" + toTitleCase($(this).val()) + "</b></p><p></p>");
+    }
+  });
+</script>
+
 <?php get_footer(); ?>
