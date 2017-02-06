a1fa711d967c96a2be7f46e617d93cfd5918eb77
Andy Carnahan <acarnahan@gmail.com>
2017-02-06 12:55:50 +0100

    make entry headers colorful

--- archive-bordr.php
+++ archive-bordr.php
@@ -128,7 +128,7 @@ get_header(); ?>
 				<div id="masonry" class="row">
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<div class="col-xs-12 masonry-item">
+					<div class="col-xs-12 col-sm-6 col-lg-4 masonry-item">
 						<?php get_template_part( 'bordrloop', get_post_format() ); ?>
 					</div>
 
