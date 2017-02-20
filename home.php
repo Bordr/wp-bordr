f0b6d6e8df3c9d6736b7d1b4236167c07f3ee7ff
Andy Carnahan <acarnahan@gmail.com>
2017-02-20 16:32:42 +0100

    fix responsive video container

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 get_header(); ?>
 
 <div class="row">
-		<iframe src="https://player.vimeo.com/video/200638965" class="intro-video" width="960" height="540" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://player.vimeo.com/video/200638965' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
 </div>
 
 <div class="row">
